8405bf0682a125e1cf7c45e6aa62cd7dd019e732
Devendra Gupta <guptadevendra7432@gmail.com>
2023-03-02 13:26:39 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import './App.css';
-import Search from './components/search';
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "enter api key here",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+ 
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
   return (
- <>
+    <div className="App">
+      <header className="App-header">
+ 
+        <h1>Weather App</h1>
 
-<Search/>
+  
+        <div>
+          <input
+            type="text"
+            placeholder="Enter name of city..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
 
- </>
+   
+        {typeof weather.main !== "undefined" ? (
+          <div>
+        
+            <p>Weather in the city:{weather.name}</p>
+            <p>temperature:{weather.main.temp}°C</p>
+            <p>Humidity:{weather.main.humidity}</p>
+            <p>Sea level:{weather.main.sea_level}</p>
+              <p>Ground level:{weather.main.grnd_level}</p>
+          </div>
+        ) : (
+          <h3>Enter valid city</h3>
+        )}
+      </header>
+    </div>
   );
 }
 
